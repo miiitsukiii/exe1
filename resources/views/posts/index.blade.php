5653d8ae5b0478c3847f3794c1355f2625d32f90
miiitsukiii <sugar.mtk310@outlook.com>
2022-10-23 22:48:26 +0000

    8-2実装(改)

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -11,10 +11,8 @@
         <div class='posts'>
             @foreach ($posts as $post)
                 <div class='post'>
-                    <h2 class='title'>
-                        <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
-                        </h2>
-                    <p class='body'>This is a sample body.</p>
+                    <h2 class='title'>{{ $post->title }}</h2>
+                    <p class='body'>{{ $post->body }}</p>
                 </div>
             @endforeach
         </div>
